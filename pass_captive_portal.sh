5e4be65a74040fb6d977edb5818927913aee0d74
Martin Schobert <martin@schobert.cc>
2017-07-28 14:10:13 +0200

    initial import

---
+++ pass_captive_portal.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# Helper script to pass captive portals at places
+# where you stay frequently.
+#
+# Author: Martin Schobert <martin@weltregierung.de>
+#
+
+
+PROGRAM_DIR="$(dirname "$0")"
+SCRIPT_DIR=$PROGRAM_DIR/scripts
+
+# load configuration
+
+if [ -f "$PROGRAM_DIR/config.default" ]; then
+    . "$PROGRAM_DIR/config.default"
+fi
+
+if [ -f "$PROGRAM_DIR/config" ]; then
+    . "$PROGRAM_DIR/config"
+fi
+
+if [ "$DEVICE" = "" ]; then
+    echo "+ Failed to load a config file"
+    exit
+fi
+
+echo "+ Using WiFi device: $DEVICE"
+
+MAC_ADDRESS=$(cat /sys/class/net/$DEVICE/address)
+echo "+ Current MAC address is: $MAC_ADDRESS"
+
+
+# Get current WiFi network
+CURRENT_SSID=`iwgetid $DEVICE -r`
+echo "+ Current SSID is: $CURRENT_SSID"
+
+
+# check if WiFi network is known
+if [ "$CURRENT_SSID" = "WIFIonICE" ] ; then
+    echo "+ Lauch script"
+    $SCRIPT_DIR/wifionice.sh
+elif [ "$CURRENT_SSID" = "FREE MEE" ] ; then
+    echo "+ Lauch script"
+    $SCRIPT_DIR/free_mee.sh $DEVICE $MAC_ADDRESS
+else
+    echo "+ Unkown SSID, will be ignored"
+    exit
+fi
+
+if [ $? = 0 ]; then
+    echo "+ Script was successful."
+else
+    echo "+ Error: Script failed. Sopping here."
+    exit
+fi
+
+# check success
+echo "+ Send test request to: $URL_TO_TEST"
+curl --silent  "$URL_TO_TEST" | grep "$EXPECTED_STR"
+if [ $? = 0 ]; then
+    echo "+ Success: Captive portal passed"
+else
+    echo "+ Error: Captive portal not passed"
+fi
---
+++ scripts/wifionice.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Author: Jan Girlich (@vollkorn1982)
+# 
+curl -s "https://www.ombord.info/hotspot/hotspot.cgi?connect=&method=login&realm=db_advanced_wifi"
